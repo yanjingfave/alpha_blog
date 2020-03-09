967664f0ff7985374da15cef65000d24ba72b6c2
yanjingfave <yanjing.tan@myfave.com>
2020-03-09 22:51:42 +0800

    Add sign up part

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
 
   resources :articles
 
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+  # post 'users', to: 'users#create'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
